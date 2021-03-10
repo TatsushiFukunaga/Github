c38b279a974e0744ccfd78bae817e989cc7d2142
tatsushi.fukunaga <tatsushi1789@gmail.com>
2021-03-10 13:36:31 +0900

    Nukeを導入し、画像を取得する処理を簡素化

--- Github/Controllers/DetailViewController.swift
+++ Github/Controllers/DetailViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import Nuke
 
-class DetailViewController: UIViewController, DetailModelDelegate {
+class DetailViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
@@ -18,7 +19,6 @@ class DetailViewController: UIViewController, DetailModelDelegate {
     @IBOutlet weak var issuesLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
-    var detailModel = DetailModel()
     var repository : Repository!
     
     override func viewDidLoad() {
@@ -39,17 +39,8 @@ class DetailViewController: UIViewController, DetailModelDelegate {
     
     func setUpImage(){
         if let imageURL = URL(string: repository.owner.avatarUrl) {
-            detailModel.detailModelDelegate = self
-            detailModel.getImage(url: imageURL)
+            Nuke.loadImage(with: imageURL, into: imageView)
             imageView.layer.cornerRadius = imageView.frame.height / 2
-        }
-    }
-    
-    func dataDidUpdate(data: Data) {
-        let image = UIImage(data: data)
-        DispatchQueue.main.async { [weak self] in
-            guard let self = self else { return }
-            self.imageView.image = image
         }
     }
     
